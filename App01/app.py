fa06944504240b031e226b8632cf217fc0e2b59e
prashantjinwal <prashantjinwal888@gmail.com>
2025-12-25 16:08:26 +0530

    learn login logout session handling

--- App01/app.py
+++ App01/app.py
@@ -1,7 +1,40 @@
-from flask import Flask
+from flask import Flask, request, Response, redirect, url_for, session
 
 app = Flask(__name__)
+app.secret_key = "Supersecret"
 
-@app.route("/")
-def home():
-    return "This is my first flask app"
+@app.route("/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if username == "admin" and password == "123":
+            session["user"] = username
+            return redirect(url_for('welcome'))
+        else :
+            return Response("In-valid candentials. Try again", mimetype="text/plain")
+        
+    return '''
+        <h2>Login page</h2>
+        <form method="POST">
+        Username : <input type="text" name="username"><br>
+        Password : <input type="text" name="password"><br>
+        <input type="submit" value="Login">
+        </form>
+
+'''
+
+@app.route("/welcome")
+def welcome():
+    if "user" in session:
+        return f''' 
+        <h2>Welcome {session["user"]} !</h2>
+        <a href={url_for('logout')}> Logout </a>
+'''
+    return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
