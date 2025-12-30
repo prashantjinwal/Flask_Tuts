851e31bfdff6e8da699a4bea8d56a99e2b31d2de
prashantjinwal <prashantjinwal888@gmail.com>
2025-12-30 21:27:41 +0530

    Add form handling

---
+++ App04/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, redirect, flash, url_for, render_template
+from forms import RegistrationForm
+
+app = Flask(__name__)
+app.secret_key = "my-secret-key"
+
+@app.route("/", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        flash(f"Welcome {name}. you register successfuly !!", "success")
+        return redirect(url_for("success"))
+    return render_template("register.html", form=form)
+
+@app.route("/success")
+def success():
+    return render_template("success.html")
