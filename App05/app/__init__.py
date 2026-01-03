6fa97600a49f0203c211cd5beb9a499bc9fbdccf
prashantjinwal <prashantjinwal888@gmail.com>
2026-01-03 19:06:23 +0530

    Add chanhges

--- App05/app/__init__.py
+++ App05/app/__init__.py
@@ -7,19 +7,19 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
 
-    # ✅ REQUIRED for session, login, flash
+    # REQUIRED for session, login, flash
     app.config['SECRET_KEY'] = 'your-secret-key'
 
     # Database configuration
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    # ✅ Correct way to initialize db
+    # initialize db
     db.init_app(app)
 
     # Import blueprints
     from app.routes.auth import auth_bp
-    from app.routes.task import task_bp   # <-- filename must be tasks.py
+    from app.routes.task import task_bp   
 
     # Register blueprints
     app.register_blueprint(auth_bp)
